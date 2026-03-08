5993f9b8726213bc87d11f2843281b18dbd07fae
pedrotski <58076186+pedrotski@users.noreply.github.com>
2026-03-08 12:31:03 +0800

    Revert "Patch for null messages"
    This reverts commit 32939384caae0709ed9361f73fb4611a00d43ae0.

--- src/TServer.cpp
+++ src/TServer.cpp
@@ -472,12 +472,7 @@ void TServer::ParseVehicle(TClient& c, const std::string& Pckt, TNetwork& Networ
         }
 
         if (PID != -1 && VID != -1 && PID == c.GetID()) {
-            auto BracePos = Data.find('{');
-            if (BracePos == std::string::npos) {
-                beammp_debug("Invalid 'Or' packet from " + c.GetName() + ": missing '{' in data");
-                return;
-            }
-            Data = Data.substr(BracePos);
+            Data = Data.substr(Data.find('{'));
             LuaAPI::MP::Engine->ReportErrors(LuaAPI::MP::Engine->TriggerEvent("onVehicleReset", "", c.GetID(), VID, Data));
             Network.SendToAll(&c, StringToVector(Packet), false, true);
         }
@@ -506,12 +501,7 @@ void TServer::ParseVehicle(TClient& c, const std::string& Pckt, TNetwork& Networ
         }
 
         if (PID != -1 && VID != -1 && PID == c.GetID()) {
-            auto BracketPos = Data.find('[');
-            if (BracketPos == std::string::npos) {
-                beammp_debug("Invalid 'Op' packet from " + c.GetName() + ": missing '[' in data");
-                return;
-            }
-            Data = Data.substr(BracketPos);
+            Data = Data.substr(Data.find('['));
             LuaAPI::MP::Engine->ReportErrors(LuaAPI::MP::Engine->TriggerEvent("onVehiclePaintChanged", "", c.GetID(), VID, Data));
             Network.SendToAll(&c, StringToVector(Packet), false, true);
 
